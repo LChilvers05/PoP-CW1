890c91c9a9b5f83614bfa304bcb1049b097ff2b5
Lee Chilvers <lee.chilvers@icloud.com>
2021-02-09 18:51:07 +0000

    •basic logic for starting server

--- ChatServer.java
+++ ChatServer.java
@@ -1,32 +1,65 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 class ChatServer {
 
+  private ServerSocket socket;
 
-
-
-  //gets the port number from console arguments
-  private int getPort(String[] args) {
-    if (args.length == 2) {
-      boolean portFlag = false;
-      for (String arg : args) {
-        if (portFlag) {
-          try{
-            int port = Integer.parseInt(arg);
-            return port;
-          } catch (NumberFormatException ex){
-            System.out.println("Invalid port argument - using port 14001");
-            break;
-          }
-        } else if (arg.equals("-csp")) {
-          portFlag = true;
-        }
-      }
+  public ChatServer(int port) {
+    try {
+      socket = new ServerSocket(port);
+    } catch (IOException e) {
+      e.printStackTrace();
     }
-    return 14001;
   }
 
+  public void startServer() {
+    println("Server started");
+    try {
+      //accept client connection
+      Socket clientSocket = socket.accept();
+      println("Connection on: " + socket.getLocalPort() + " ; " + clientSocket.getPort());
+
+      //to read data from the client
+      InputStreamReader clientCharStream = new InputStreamReader(clientSocket.getInputStream());
+      BufferedReader clientIn = new BufferedReader(clientCharStream);
+
+      //to send data to the client
+      PrintWriter clientOut = new PrintWriter (clientSocket.getOutputStream(), true);
+
+      //read and send to client
+      while(true) {
+        String userInput = clientIn.readLine();
+        clientOut.println(userInput);
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      stopServer();
+    }
+  }
+
+  /**
+   * closes server socket connection
+   */
+  private void stopServer() {
+    try {
+      socket.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 
   public static void main(String[] args) {
-    ChatServer chatServer = new ChatServer();
-    int port = chatServer.getPort(args);
+    ChatServer chatServer = new ChatServer(PortHelper.getPort(args));
+  }
+
+  //helper functions
+  public static void println(String msg) {
+    System.out.println(msg);
   }
 }
